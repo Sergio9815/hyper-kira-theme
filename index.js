b65672dd70779332ede6a57c1e15fdcfa9f4dc01
sergio9815 <gsergio1598@gmail.com>
2021-06-23 20:54:16 -0500

    :art: Adding colors

--- index.js
+++ index.js
@@ -1,30 +1,45 @@
+//COLORS
+const skyBlue = '#57C7FF';
+const lgBlue = '#6DDCCF';
+const black = '#000000';
+const dkGrey = '#333';
+const lgGrey = '#555555';
+const red = '#F56A79';
+const rose = '#F25287';
+const purple = '#A685E2';
+const yellow = '#FFEDA3';
+const lgYellow = '#FDFFB6';
+const white = '#fff';
+const green = '#A5E1AD';
+const lgGreen = '#9BDEAC';
+
 module.exports.decorateConfig = (config) => {
   return Object.assign({}, config, {
-    cursorColor: '#57C7FF',
+    cursorColor: skyBlue,
     cursorShape: 'UNDERLINE',
-    cursorAccentColor: '#000000',
-    foregroundColor: '#fff',
-    backgroundColor: '#000000',
-    selectionColor: '#57C7FF',
-    borderColor: '#333',
+    cursorAccentColor: black,
+    foregroundColor: white,
+    backgroundColor: black,
+    selectionColor: skyBlue,
+    borderColor: dkGrey,
     fontWeightBold: 'normal',
     colors: {
-      black: '#000000',
-      red: '#F56A79',
-      green: '#A5E1AD',
-      yellow: '#FFEDA3',
-      blue: '#000000',
-      magenta: '#F25287',
-      cyan: '#6DDCCF',
-      white: '#FFFFFF',
-      lightBlack: '#555555',
-      lightRed: '#FFEDA3',
-      lightGreen: '#9BDEAC',
-      lightYellow: '#FDFFB6',
-      lightBlue: '#57C7FF',
-      lightMagenta: '#A685E2',
-      lightCyan: '#57C7FF',
-      lightWhite: '#FFFFFF',
+      black: black,
+      red: red,
+      green: green,
+      yellow: yellow,
+      blue: black,
+      magenta: rose,
+      cyan: lgBlue,
+      white: white,
+      lightBlack: lgGrey,
+      lightRed: yellow,
+      lightGreen: lgGreen,
+      lightYellow: lgYellow,
+      lightBlue: skyBlue,
+      lightMagenta: purple,
+      lightCyan: skyBlue,
+      lightWhite: white,
     },
   })
 }
